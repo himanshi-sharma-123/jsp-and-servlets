34a7761852fc17f79b7f27bc51f8bb26449ea86a
Himanshi Sharma <himanshish456@gmail.com>
2024-08-16 16:33:25 +0530

    jsp and servlets

---
+++ ServletFirstDemo/src/main/java/backend/MyServlet.java
@@ -0,0 +1,17 @@
+package backend;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.print("I am in doGet() method for browser");
+        System.out.println("I am in doGet() method");
+    }
+}
